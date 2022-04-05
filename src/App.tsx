866cccd161d4a9f892191cdd339418b2e71ec268
idkbit <ggwpanda@gmail.com>
2022-04-05 14:50:51 +0600

    init

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+import { useState } from "react";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return <div className="App text-8xl text-primaryNG text-qoute">app</div>;
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        primaryLC: "hsl(193, 38%, 86%)",
+        primaryNG: "hsl(150, 100%, 66%)",
+        neutralGB: "hsl(217, 19%, 38%)",
+        neutralDGB: "hsl(217, 19%, 24%)",
+        neutralDB: "hsl(218, 23%, 16%)",
+      },
+      fontSize: {
+        qoute: "1.75rem",
+      },
+    },
+  },
+  plugins: [],
+};
